b375dac418ae4efa413efdbfd9df403874a5b339
prathambhasindrive <prathambhasindrive@gmail.com>
2024-05-07 09:35:02 +0530

    added components and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+ import { BrowserRouter as Router , Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Services } from './pages/Services';
+import { Contact } from './pages/Contact';
+import { Signup } from './pages/Signup';
+import { Login } from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Router>
+        
+          <Route exact path='/' Component={Home}></Route>
+          <Route path='/about' Component={About}></Route>
+          <Route path='/services' Component={Services}></Route>
+          <Route path='/contact' Component={Contact}></Route>
+          <Route path='/signup' Component={Signup}></Route>
+          <Route path='/login' Component={Login}></Route>
+        
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
+ 
 const Navbar = () => {
   return (
     <nav className="bg-gray-400 w-full h-12 flex items-center ">
@@ -7,24 +8,24 @@ const Navbar = () => {
         <div className="text-white font-bold mx-2 my-1 text-2xl cursor-pointer">Budgetify</div>
         <ul className="hidden md:flex border-box">
           <li className="text-blue-900 font-bold mx-2 my-1 hover:border-b-2 hover:border-blue-900 hover:border-solid">
-            <a href="">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li className="text-blue-900 font-bold mx-2 my-1 hover:border-b-2 hover:border-blue-900 hover:border-solid">
-            <a href="">Services</a>
+            <Link to="/services">Services</Link>
           </li>
           <li className="text-blue-900 font-bold mx-2 my-1 hover:border-b-2 hover:border-blue-900 hover:border-solid">
-            <a href="">About Us</a>
+            <Link to="/about">About Us</Link>
           </li>
           <li className="text-blue-900 font-bold mx-2 my-1 hover:border-b-2 hover:border-blue-900 hover:border-solid">
-            <a href="">Contact Us</a>
+            <Link to="/contact">Contact Us</Link>
           </li>
         </ul>
         <div>
           <button className="bg-blue-600 px-2 py-1 mx-2 rounded-md text-white hover:bg-blue-500 hidden md:inline ">
-            <a href="">Sign Up</a>
+            <link to="/signup">Sign Up</link>
           </button>
           <button className="bg-blue-600 px-2 py-1 mx-2 rounded-md text-white hover:bg-blue-500  md:inline">
-            <a href="">Log In</a>
+            <link to="login">Log In</link>
           </button>
         </div>
       </div>
