af91381ac3066dad20ebad0bf34689dfacd0b2e4
ayushikamboz <147229792+ayushikamboz@users.noreply.github.com>
2024-05-09 00:05:36 +0530

    signup div creation

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Signup = () => {
     return ( 
-        <div>
-            <h1>Signup</h1>
+        <div className="h-screen w-screen bg-gradient-to-r from-violet-500 to-fuchsia-500 flex   justify-evenly  ">
+            
+
+            <div className="w-3/5 h-4/5 flex  flex-row   mt-16 shadow-2xl justify-evenly bg-white" >
+              
+              <div className="  w-2/5 h-4/5 flex flex-column border-black border-2 justify-evenly  mt-8 ">
+              <h1 className=" ">Signup</h1> 
+              </div>
+              <div className="w-2/5 h-4/5 flex flex-column border-black border-2 justify-evenly mt-8  ">
+
+              </div>
+            </div>
+            
         </div>
      );
 }
