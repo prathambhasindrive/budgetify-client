194421bcfb5c1dd18e3f1c8e04b63483d34b5c70
Ayushi kamboj <ayushikamboj0007@gmail.com>
2024-05-08 18:53:52 +0530

    signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Signup = () => {
     return ( 
-        <div>
-            <h1>Signup</h1>
+        <div className='flex w-screen h-screen  bg-cyan-500 '>
+            
+            <h1 className='justify-center'>Signup</h1>
+            <div className='innerDiv flex flex-row border-2  border-black h-4/5 w-4/5 my-7'>
+            <div className=" leftDiv flex flex-column bg-sky-500 h-3/6 w-3/6 my-7 ">
+                
+            </div>
+            <div className='rightDiv flex flex-column bg-sky-500 h-3/6 w-3/6 my-7 mx-7'>
+
+            </div>
+            </div>
         </div>
      );
 }
