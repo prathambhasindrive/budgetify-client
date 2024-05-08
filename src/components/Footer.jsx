f14e884f46137cc07307ed988207c1adc22c33a4
prathambhasindrive <prathambhasindrive@gmail.com>
2024-05-08 16:57:58 +0530

    addef footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Footer = () => {
+    return ( 
+    <>
+     <nav className="bg-gray-400 w-full h-12 text-center flex justify-center items-center text-white font-bold">
+            Copyrights ©️ Budgetify || 2024 , All Rights Reserved
+      </nav>
+    </> 
+    );
+}
+ 
+export default Footer;<>
+
+</>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Herosection from '../components/Herosection';
 import Featuresection from '../components/Featuresection';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return ( 
         <>
            <Herosection /> 
            <Featuresection />
+
+           <Footer />
         </>
      );
 }
