b81598089cb7556af43ffb84970b82702732610c
Ayushi kamboj <ayushikamboj0007@gmail.com>
2024-05-10 15:42:28 +0530

    signup first

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,20 +2,35 @@ import React from 'react';
 
 const Signup = () => {
     return ( 
-        <div className="h-screen w-screen bg-gradient-to-r from-violet-500 to-fuchsia-500 flex   justify-evenly  ">
+        <section className=" min-h-screen bg-gray-100 flex justify-center items-center ">
             
 
-            <div className="w-3/5 h-4/5 flex  flex-row   mt-16 shadow-2xl justify-evenly bg-white" >
+            <div className="bg-gray-200  rounded-3xl shadow-lg max-w-3xl flex " >
               
-              <div className="  w-2/5 h-4/5 flex flex-column border-black border-2 justify-evenly  mt-8 ">
-              <h1 className=" ">Signup</h1> 
-              </div>
-              <div className="w-2/5 h-4/5 flex flex-column border-black border-2 justify-evenly mt-8  ">
+              <div className=" flex flex-col bg-cyan-200 justify-center items-center w-1/2 flex-wrap ">
+                    <h1 className=' mt-2  font-bold text-2xl'>Signup</h1>
+                    <form  className= " flex  flex-col " action=''>
+                      <h3 className=' mt-5 '>First Name</h3>
+                      <input  className=' rounded-lg p-2 mt-2' type='text' name='firstName' placeholder='Enter your first name'/> 
 
+                      <h3 className=' mt-3 '>Last Name</h3>
+                      <input className=' rounded-lg p-2  mt-2' type='text' name="last Name" placeholder='Enter your last name'/>
+                      <h3 className=' mt-3'>Email ID</h3>
+                    <input className=' rounded-lg  p-2 mt-2' type='text' name="Email" placeholder='Enter your email'/>
+                    <h3 className=' mt-3'>Password</h3>
+                      <input className=' rounded-lg   p-2 mt-2' type='password' name="password" placeholder='password'/>
+                      <h3 className=' mt-3' >Confirm Password</h3>
+                      <input className=' rounded-lg   mt-2 p-2' type='password' name="confirm password" placeholder='Confirm password'/>
+                       <button className='  flex justify-center  items-center rounded-lg  w-5/12  h-6  mt-5 mb-5  border-2 '>SignUp</button>
+                    </form>
+                
+              </div>
+              <div className=" w-1/2  sm:block hidden p-5 bg-cyan-100 ">
+                      <img className="  rounded-3xl" src='\assets\images\investment-portfolio-feature.png'alt=''/>
               </div>
             </div>
             
-        </div>
+        </section>
      );
 }
  
