ff58933299e7c524d1cd507b768bead4f39ad0bc
Ayushi kamboj <ayushikamboj0007@gmail.com>
2024-05-10 16:42:13 +0530

    signup  second

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,23 +5,35 @@ const Signup = () => {
         <section className=" min-h-screen bg-gray-100 flex justify-center items-center ">
             
 
-            <div className="bg-gray-200  rounded-3xl shadow-lg max-w-3xl flex " >
+            <div className="bg-gray-200  sm:bg-cyan-200 rounded-3xl shadow-lg max-w-3xl flex " >
               
               <div className=" flex flex-col bg-cyan-200 justify-center items-center w-1/2 flex-wrap ">
-                    <h1 className=' mt-2  font-bold text-2xl'>Signup</h1>
+                    <label className=' mt-2  font-bold text-2xl'>Signup</label>
                     <form  className= " flex  flex-col " action=''>
-                      <h3 className=' mt-5 '>First Name</h3>
+                      <div className='flex flex-col'>
+                      
+                      <label className=' mt-5 '>First Name</label>
                       <input  className=' rounded-lg p-2 mt-2' type='text' name='firstName' placeholder='Enter your first name'/> 
-
-                      <h3 className=' mt-3 '>Last Name</h3>
+                      </div>
+                      <div className='flex flex-col'>
+                      <label className=' mt-3 '>Last Name</label>
                       <input className=' rounded-lg p-2  mt-2' type='text' name="last Name" placeholder='Enter your last name'/>
-                      <h3 className=' mt-3'>Email ID</h3>
+                      </div>
+                      <div className='flex flex-col'>
+                      <label className=' mt-3'>Email ID </label>
                     <input className=' rounded-lg  p-2 mt-2' type='text' name="Email" placeholder='Enter your email'/>
-                    <h3 className=' mt-3'>Password</h3>
+                    </div>
+                    <div className='flex flex-col'>
+                    <label className=' mt-3'>Password</label>
                       <input className=' rounded-lg   p-2 mt-2' type='password' name="password" placeholder='password'/>
-                      <h3 className=' mt-3' >Confirm Password</h3>
+                      </div>
+                      <div className='flex flex-col'>
+                      <label className=' mt-3' >Confirm Password</label>
                       <input className=' rounded-lg   mt-2 p-2' type='password' name="confirm password" placeholder='Confirm password'/>
-                       <button className='  flex justify-center  items-center rounded-lg  w-5/12  h-6  mt-5 mb-5  border-2 '>SignUp</button>
+                      </div>
+                      <div className='flex flex-col justify-center items-center'>
+                       <button className='  shadow-lg flex items-center justify-center  hover:bg-cyan-100 rounded-lg  w-6/12  h-6  mt-5 mb-5  border-2 '>SignUp</button>
+                       </div>
                     </form>
                 
               </div>
